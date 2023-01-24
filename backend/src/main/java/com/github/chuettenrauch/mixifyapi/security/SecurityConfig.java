d545b4eea16496f80231da082f82dad3da674592
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-01-24 18:45:12 +0100

    GH-9 - added oauth2 config for spotify and adjust security config accordingly

--- backend/src/main/java/com/github/chuettenrauch/mixifyapi/security/SecurityConfig.java
+++ backend/src/main/java/com/github/chuettenrauch/mixifyapi/security/SecurityConfig.java
@@ -2,19 +2,34 @@ package com.github.chuettenrauch.mixifyapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    // temporarily disable spring security
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
-                .authorizeHttpRequests(authorize ->
-                        authorize.anyRequest().permitAll()
+                .cors().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/oauth2/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .redirectionEndpoint(config -> config
+                                .baseUri("/oauth2/code/*")
+                        )
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
                 .build();
     }
