7d07e3085a012439c94a044e050a5751059d51df
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-01-27 11:56:53 +0100

    GH-32 - added missing track model

---
+++ backend/src/main/java/com/github/chuettenrauch/mixifyapi/mixtape/model/Track.java
@@ -0,0 +1,16 @@
+package com.github.chuettenrauch.mixifyapi.mixtape.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Track {
+    @Id
+    private String id;
+}
