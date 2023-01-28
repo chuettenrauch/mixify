e56dc73bd30210d2d76dc043669871eca16e9f1d
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-01-28 10:55:48 +0100

    GH-35 - added fab for adding mixtapes

--- frontend/src/pages/MixtapesOverviewPage.tsx
+++ frontend/src/pages/MixtapesOverviewPage.tsx
@@ -1,8 +1,9 @@
-import {Container, Stack, Typography} from "@mui/material";
+import {Container, Fab, Stack, Typography} from "@mui/material";
 import Mixtape from "../types/mixtape";
 import {useEffect, useState} from "react";
 import {MixtapeApi} from "../api/mixify-api";
 import MixtapeCard from "../components/MixtapeCard";
+import {Add as AddIcon} from "@mui/icons-material";
 
 export default function MixtapesOverviewPage() {
     const [mixtapes, setMixtapes] = useState<Mixtape[]>([]);
@@ -15,20 +16,30 @@ export default function MixtapesOverviewPage() {
     }, []);
 
     return (
-        <Container sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: 3,
-            p: 0,
-        }}>
-            <Typography variant="h1" component="h1" textTransform={"uppercase"}>Your Mixtapes</Typography>
+        <>
+            <Container sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: 3,
+                p: 0,
+            }}>
+                <Typography variant="h1" component="h1" textTransform={"uppercase"}>Your Mixtapes</Typography>
 
-            <Stack spacing={2} sx={{width: "100%"}}>
-                {mixtapes.map(mixtape => (
-                    <MixtapeCard key={mixtape.id} mixtape={mixtape}/>
-                ))}
-            </Stack>
-        </Container>
+                <Stack spacing={2} sx={{width: "100%"}}>
+                    {mixtapes.map(mixtape => (
+                        <MixtapeCard key={mixtape.id} mixtape={mixtape}/>
+                    ))}
+                </Stack>
+            </Container>
+
+            <Fab color="primary" size="medium" aria-label="add" sx={{
+                position: "fixed",
+                bottom: (theme) => theme.spacing(9),
+                right: (theme) => theme.spacing(2)}
+            }>
+                <AddIcon/>
+            </Fab>
+        </>
     );
 }
