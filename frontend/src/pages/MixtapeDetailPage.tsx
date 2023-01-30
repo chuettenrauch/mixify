b20096ced843335f33c86d6b2525f40603ab518e
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-01-30 16:44:10 +0100

    GH-48 - added skeleton for tracks list

--- frontend/src/pages/MixtapeDetailPage.tsx
+++ frontend/src/pages/MixtapeDetailPage.tsx
@@ -4,7 +4,8 @@ import {MixtapeApi} from "../api/mixify-api";
 import PageHeader from "../components/PageHeader";
 import Mixtape from "../types/mixtape";
 import MixtapeDetails from "../components/MixtapeDetails";
-import { Container } from "@mui/material";
+import {Container, Divider, Fab, Typography} from "@mui/material";
+import {Add as AddIcon, Info as InfoIcon} from "@mui/icons-material";
 
 export default function MixtapeDetailPage() {
     const navigate = useNavigate();
@@ -35,13 +36,33 @@ export default function MixtapeDetailPage() {
         <Container sx={{
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
             gap: 3,
             p: 0,
         }}>
             <PageHeader title={mixtape.title}/>
 
             <MixtapeDetails mixtape={mixtape} onEdit={(savedMixtape: Mixtape) => setMixtape(savedMixtape)} onDelete={navigateToMixtapesOverviewPage}/>
+            <Divider sx={{width: "100%"}}/>
+
+            <Typography variant="h2" component="h2" textTransform={"uppercase"}>Tracks</Typography>
+            <Container sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", minHeight: 200}}>
+                {mixtape.tracks.length === 0
+                    ? <>
+                        <Typography>Your mixtape has no tracks yet.</Typography>
+                        <Typography sx={{display: "flex", alignItems: "center"}}><InfoIcon color="primary"/> You can add up to 12 tracks.</Typography>
+                    </>
+                    : <></>
+                }
+            </Container>
+
+            <Fab color="primary" size="medium" aria-label="add track" sx={{
+                position: "fixed",
+                bottom: (theme) => theme.spacing(9),
+                right: (theme) => theme.spacing(2)}
+            }>
+                <AddIcon/>
+            </Fab>
+
         </Container>
     );
 }
