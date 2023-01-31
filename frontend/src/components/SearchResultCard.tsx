cced3f742b04a188a339b51399dad0ef63eb1614
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-01-31 10:32:54 +0100

    GH-51 - added possibility to play song preview

--- frontend/src/components/SearchResultCard.tsx
+++ frontend/src/components/SearchResultCard.tsx
@@ -1,28 +1,82 @@
 import {
+    Box,
     Card,
-    CardActionArea,
+    CardActionArea, CardActions,
     CardContent,
-    CardMedia,
+    CardMedia, IconButton,
     Typography
 } from "@mui/material";
-import React from "react";
+import React, {useRef, useState} from "react";
+import {PlayCircle as PlayCircleIcon, PauseCircle as PauseCircleIcon} from "@mui/icons-material";
+import formatDuration from "format-duration";
+
+declare global {
+    namespace Spotify {
+        interface Track {
+            preview_url: string;
+        }
+    }
+}
 
 export default function SearchResultCard({searchResult}: {
     searchResult: Spotify.Track,
 }) {
-    return (
-        <Card elevation={5} sx={{display: "flex"}}>
-            <CardActionArea sx={{display: "flex", justifyContent: "flex-start", alignItems: "stretch", p: 2}}>
-                <CardMedia
-                    component="img"
-                    image={searchResult.album.images[0]?.url}
-                    alt={searchResult.name}
-                    sx={{width: 100, height: 100, lineHeight: 0, border: "1px solid grey"}}
-                />
+    const player = useRef<HTMLAudioElement | null>(null);
+    const [playing, setPlaying] = useState<boolean>(false);
 
-                <CardContent sx={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
-                    <Typography variant="h3">{searchResult.name}</Typography>
-                    <Typography>{searchResult.artists[0]?.name}</Typography>
+    const onPlay = () => {
+        if (player.current?.paused) {
+            setPlaying(true);
+            player.current?.play();
+        } else {
+            setPlaying(false);
+            player.current?.pause();
+        }
+    }
+
+    return (
+        <Card elevation={5} sx={{display: "flex", p: 2}}>
+            <CardActions sx={{p: 0, position: "relative"}}>
+                    <IconButton
+                        size="large"
+                        onClick={onPlay}
+                        sx={{
+                            display: "block",
+                            m: 0,
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            zIndex: 1
+                        }}
+                    >
+                        <audio ref={player} hidden>
+                            <source src={searchResult.preview_url} type="audio/mpeg"/>
+                        </audio>
+                        {playing
+                            ? <PauseCircleIcon sx={{width: "100%", height: "100%"}}/>
+                            : <PlayCircleIcon sx={{width: "100%", height: "100%"}}/>
+                        }
+                    </IconButton>
+                    <CardMedia
+                        component="img"
+                        image={searchResult.album.images[0]?.url}
+                        alt={searchResult.name}
+                        sx={{width: 100, height: 100, lineHeight: 0, border: "1px solid grey", opacity: 0.7}}
+                    />
+            </CardActions>
+
+            <CardActionArea sx={{display: "flex", justifyContent: "flex-start", alignItems: "stretch", p: 0}}>
+                <CardContent sx={{display: "flex", flexDirection: "column", justifyContent: "space-between", p: 0, pl: 2}}>
+                    <Box>
+                        <Typography variant="h3">{searchResult.name}</Typography>
+                        <Typography>{searchResult.artists[0]?.name}</Typography>
+                        <Typography>{formatDuration(searchResult.duration_ms)}</Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="caption">Album: {searchResult.album.name}</Typography>
+                    </Box>
                 </CardContent>
             </CardActionArea>
         </Card>
