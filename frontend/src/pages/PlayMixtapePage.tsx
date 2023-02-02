68e6f13bcb2cf57bef8773e0da093828033d6d6d
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-02-02 17:15:34 +0100

    GH-71 - added card flip

--- frontend/src/pages/PlayMixtapePage.tsx
+++ frontend/src/pages/PlayMixtapePage.tsx
@@ -1,14 +1,15 @@
 import {Location, useLocation, useParams} from "react-router-dom";
 import useMixtape from "../hooks/useMixtape";
 import localStorage from "react-secure-storage";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import StorageKey from "../utils/local-storage-utils";
 import useMixtapePlayer from "../hooks/useMixtapePlayer";
-import {Box, CardMedia, Container, IconButton, Typography} from "@mui/material";
+import {Box, Card, CardMedia, Container, IconButton, Typography} from "@mui/material";
 import PageHeader from "../components/PageHeader";
-import {PlayCircle as PlayCircleIcon} from "@mui/icons-material";
+import {Redo as RedoIcon, PlayCircle as PlayCircleIcon} from "@mui/icons-material";
 import UserAvatar from "../components/UserAvatar";
 import MixtapeUtils from "../utils/mixtape-utils";
+import ReactCardFlip from "react-card-flip";
 
 export default function PlayMixtapePage() {
     const location = useLocation();
@@ -16,6 +17,7 @@ export default function PlayMixtapePage() {
     const {mixtape} = useMixtape(id);
 
     const {playerReady, addTracksToPlayer, startPlayer, pausePlayer} = useMixtapePlayer();
+    const [isImageFlipped, setIsImageFlipped] = useState<boolean>(false);
 
     useEffect(() => {
         updateLastPlayUrlInLocalStorage(location);
@@ -55,47 +57,87 @@ export default function PlayMixtapePage() {
         }}>
             <PageHeader title={mixtape.title}/>
 
-            <Box sx={{
-                height: 0,
-                overflow: "hidden",
-                width: "100%",
-                paddingTop: "100%",
-                position: "relative",
-                border: "1px solid grey",
-            }}>
-                <CardMedia
-                    component="img"
-                    image={mixtape.imageUrl}
-                    alt={mixtape.title}
-                    sx={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        objectFit: "cover",
-                        width: "100%",
-                        height: "100%"
-                    }}
-                />
-                {playerReady &&
-                  <IconButton size="large" component="button" onClick={() => startPlayer()} aria-label="play" sx={{
-                      display: "block",
-                      m: 0,
-                      position: "absolute",
-                      top: 0,
-                      left: 0,
-                      width: "100%",
-                      height: "100%",
-                      zIndex: 1
-                  }}>
-                    <PlayCircleIcon sx={{width: "70%", height: "100%"}}/>
-                  </IconButton>
-                }
-            </Box>
+                <ReactCardFlip isFlipped={isImageFlipped} flipSpeedBackToFront={1} flipSpeedFrontToBack={1} containerStyle={{width: "100%"}}>
+                    <Card elevation={10} sx={{display: "flex", flexDirection: "column", width: "100%", p: 2, pb: 0}}>
+                        <Container sx={{paddingTop: "100%", position: "relative"}}>
+                            <CardMedia
+                                component="img"
+                                image={mixtape.imageUrl}
+                                alt={mixtape.title}
+                                sx={{
+                                    position: "absolute",
+                                    top: 0,
+                                    left: 0,
+                                    objectFit: "cover",
+                                    width: "100%",
+                                    height: "100%",
+                                    border: "1px solid grey",
+                                }}
+                            />
+                            {playerReady &&
+                              <IconButton size="large" component="button" onClick={() => startPlayer()} aria-label="play" sx={{
+                                  display: "block",
+                                  m: 4,
+                                  position: "absolute",
+                                  top: 0,
+                                  left: 0,
+                                  width: "80%",
+                                  height: "80%",
+                                  zIndex: 1,
+                              }}>
+                                <PlayCircleIcon sx={{width: "70%", height: "100%"}}/>
+                              </IconButton>
+                            }
+                        </Container>
+
+                        <IconButton onClick={() => setIsImageFlipped(!isImageFlipped)} sx={{marginLeft: "auto"}}>
+                            <RedoIcon/>
+                        </IconButton>
+                    </Card>
+
+                    <Card elevation={10} square={true} sx={{display: "flex", flexDirection: "column", width: "100%", p: 2, pb: 0}}>
+                        <Container sx={{paddingTop: "100%", position: "relative"}}>
+                            <CardMedia
+                                component="img"
+                                image={mixtape.imageUrl}
+                                alt={mixtape.title}
+                                sx={{
+                                    position: "absolute",
+                                    top: 0,
+                                    left: 0,
+                                    objectFit: "cover",
+                                    width: "100%",
+                                    height: "100%",
+                                    border: "1px solid grey",
+                                    opacity: 0.3,
+                                }}
+                            />
+                            <Box sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                width: "100%",
+                                height: "100%",
+                                p: 2,
+                            }}>
+                                <Typography variant="h2" textAlign="justify">{mixtape.description}</Typography>
+                            </Box>
+                        </Container>
+
+                        <IconButton onClick={() => setIsImageFlipped(!isImageFlipped)} sx={{marginLeft: "auto"}}>
+                            <RedoIcon/>
+                        </IconButton>
+                    </Card>
+                </ReactCardFlip>
+
 
             <Box sx={{display: "flex", gap: 1, width: "100%"}}>
                 <UserAvatar user={mixtape.createdBy} sx={{width: 60, height: 60}}/>
                 <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
-                    <Typography variant="h2">by {mixtape.createdBy.name}</Typography>
+                    <Typography variant="h2" textTransform="uppercase">by {mixtape.createdBy.name}</Typography>
                     <Typography>{MixtapeUtils.formatCreatedAt(mixtape.createdAt)}</Typography>
                 </Box>
             </Box>
