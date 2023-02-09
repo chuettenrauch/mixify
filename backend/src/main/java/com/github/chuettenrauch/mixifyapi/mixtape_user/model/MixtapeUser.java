d11482c45b1ca1d2ebda6bf8b42a1da9c63abccb
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-02-09 15:15:43 +0100

    GH-107 - fixed sonar issue

--- backend/src/main/java/com/github/chuettenrauch/mixifyapi/mixtape_user/model/MixtapeUser.java
+++ backend/src/main/java/com/github/chuettenrauch/mixifyapi/mixtape_user/model/MixtapeUser.java
@@ -7,14 +7,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 @Document
-@CompoundIndexes({
-        @CompoundIndex(name = "user_mixtape", def = "{'user' : 1, 'mixtape': 1}", unique = true)
-})
+@CompoundIndex(name = "user_mixtape_unique", def = "{'user' : 1, 'mixtape': 1}", unique = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
