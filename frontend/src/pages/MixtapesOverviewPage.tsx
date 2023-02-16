3a999576b9e63836024796536c622fcebd5fc954
Claudia Hüttenrauch <claudia.huettenrauch@gmail.com>
2023-02-16 23:34:15 +0100

    GH-127 - added client side search for mixtapes

--- frontend/src/pages/MixtapesOverviewPage.tsx
+++ frontend/src/pages/MixtapesOverviewPage.tsx
@@ -1,19 +1,21 @@
-import {Box, Fab, Stack, Typography} from "@mui/material";
+import {Box, Fab, IconButton, Stack, TextField, Typography} from "@mui/material";
 import MixtapeCard from "../components/MixtapeCard";
-import {Add as AddIcon} from "@mui/icons-material";
+import {Add as AddIcon, Clear as ClearIcon} from "@mui/icons-material";
 import MixtapeForm from "../components/MixtapeForm";
 import useMixtapes from "../hooks/useMixtapes";
 import PageHeader from "../components/PageHeader";
 import Mixtape from "../types/mixtape";
 import {useNavigate} from "react-router-dom";
 import useForm from "../hooks/useForm";
-import React from "react";
+import React, {useState} from "react";
 import MessageContainer from "../components/MessageContainer";
+import MixtapeUtils from "../utils/mixtape-utils";
 
 export default function MixtapesOverviewPage() {
     const navigate = useNavigate();
     const [mixtapes, setMixtapes] = useMixtapes();
     const {isFormOpen: isMixtapeFormOpen, openForm: openMixtapeForm, closeForm: closeMixtapeForm} = useForm();
+    const [searchTerm, setSearchTerm] = useState<string>("");
 
     const navigateToMixtapeDetailPage = (mixtape: Mixtape) => {
         navigate(`/mixtapes/${mixtape.id}`);
@@ -32,15 +34,39 @@ export default function MixtapesOverviewPage() {
         }}>
             <PageHeader title="Your Mixtapes"/>
 
+            <TextField
+                variant="standard"
+                id="search"
+                name="search"
+                value={searchTerm}
+                placeholder="Search mixtapes..."
+                onChange={(e) => setSearchTerm(e.target.value)}
+                sx={{width: "100%"}}
+                InputProps={{
+                    endAdornment: (
+                        <>
+                            <IconButton
+                                sx={{ visibility: searchTerm ? "visible" : "hidden" }}
+                                onClick={() => setSearchTerm("")}
+                            >
+                                <ClearIcon/>
+                            </IconButton>
+                        </>
+                    ),
+                }}
+            />
+
             <Stack spacing={2} sx={{width: "100%"}}>
                 {mixtapes.length === 0
                     ? <MessageContainer>
                         <Typography>You don't have any mixtapes, yet.</Typography>
                     </MessageContainer>
-                    : mixtapes.map(mixtape => (
-                        <MixtapeCard key={mixtape.id} mixtape={mixtape} onEdit={navigateToMixtapeDetailPage}
-                                     onDelete={removeMixtapeFromList}/>
-                    ))}
+                    : mixtapes
+                        .filter(mixtape => MixtapeUtils.containsSearchTerm(mixtape, searchTerm))
+                        .map(mixtape => (
+                            <MixtapeCard key={mixtape.id} mixtape={mixtape} onEdit={navigateToMixtapeDetailPage}
+                                         onDelete={removeMixtapeFromList}/>
+                        ))}
             </Stack>
 
             <Fab color="primary" size="medium" onClick={openMixtapeForm} aria-label="add" sx={{
